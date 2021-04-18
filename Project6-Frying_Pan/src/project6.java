37cd91f4457dd446862f014f58549c5671f5eaec
gaojianqiao <gaojianqiao@192.168.101.8>
2021-04-19 00:48:51 +0800

    made by James on 19th/april but need Khalid's Help to fix the RecipeWebsite.java

--- Project6-Frying_Pan/src/project6.java
+++ Project6-Frying_Pan/src/project6.java
@@ -30,10 +30,10 @@ import org.jsoup.select.Elements;
 //april 12th
 //James
 public class project6 implements ActionListener {
-	TextField tx_dishsearch, tx_difficulty1; 
+	TextField tx_dishsearch; 
 	TextArea tx_rating, tx_duration, tx_difficulty2, tx_categories1, tx_categories2, tx_categories3, tx_recipe,
-	tx_title, tx_ingredients;
-	Button bDish;
+	tx_title, tx_ingredients, tx_overview;
+	Button bTasty, bFoodNet;
 	JFrame f;
 	String imgDish = "";
 	String imgLogo = "";
@@ -46,7 +46,6 @@ public class project6 implements ActionListener {
 	project6(){
 		f = new JFrame("Frying Pan");
 		tx_dishsearch = new TextField("Input");
-		tx_difficulty1 = new TextField("Input");
 		tx_rating = new TextArea("Rating");
 		tx_duration = new TextArea("Duration");
 		tx_difficulty2 = new TextArea("Difficulty");
@@ -56,9 +55,10 @@ public class project6 implements ActionListener {
 		tx_recipe = new TextArea("Recipe");
 		tx_ingredients = new TextArea("Ingredients");
 		tx_title = new TextArea("Title");
-		bDish = new Button("Search");
+		tx_overview = new TextArea("Overview"); 
+		bTasty = new Button("Search by Tasty");
+		bFoodNet = new Button("Search by FoodNet");
 		ldishsearch = new JLabel("Search for Dish");
-		ldiffsearch = new JLabel("Search for Difficulty");
 		lbackground = new JLabel("Background");
 		lLogo = new JLabel("LOGO");
 		limg = new JLabel("Dish Pic");
@@ -66,26 +66,25 @@ public class project6 implements ActionListener {
 		imageIconLogo = new ImageIcon();
 		
 
-		tx_dishsearch.setBounds(400,90,100,30);
-		tx_difficulty1.setBounds(400,160,100,30);
+		tx_dishsearch.setBounds(450,80,200,30);
 		tx_rating.setBounds(30,350,150,50);
 		tx_duration.setBounds(30,450,150,50);
 		tx_difficulty2.setBounds(30,550,150,50);
 		tx_categories1.setBounds(950,350,150,70);
 		tx_categories2.setBounds(950,450,150,70);
 		tx_categories3.setBounds(950,550,150,70);
-		tx_recipe.setBounds(580,330,350,400);
-		tx_ingredients.setBounds(200,330,350,400);
-		tx_title.setBounds(330,260,450,40);
-		bDish.setBounds(420,210,100,30);
-		ldishsearch.setBounds(400,50,200,50);
-		ldiffsearch.setBounds(400,120,200,50);
+		tx_recipe.setBounds(580,380,350,400);
+		tx_ingredients.setBounds(200,380,350,400);
+		tx_title.setBounds(355,200,400,40);
+		tx_overview.setBounds(200,260,730,110);
+		bTasty.setBounds(450,120,200,30);
+		bFoodNet.setBounds(450,160,200,30);
+		ldishsearch.setBounds(480,40,200,50);
 		lLogo.setBounds(30,70,320,180);
 		limg.setBounds(800,70,300,250);
 		lbackground.setBounds(0,0,1200,850);
 		
 		f.add(tx_dishsearch);
-		f.add(tx_difficulty1);
 		f.add(tx_rating);
 		f.add(tx_duration);
 		f.add(tx_difficulty2);
@@ -95,9 +94,10 @@ public class project6 implements ActionListener {
 		f.add(tx_recipe);
 		f.add(tx_ingredients);
 		f.add(tx_title);
-		f.add(bDish);
+		f.add(tx_overview); 
+		f.add(bTasty);
+		f.add(bFoodNet);
 		f.add(ldishsearch);
-		f.add(ldiffsearch);
 		f.add(lLogo);
 		f.add(limg);
 		f.add(lbackground);
@@ -108,7 +108,8 @@ public class project6 implements ActionListener {
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
 		
-		bDish.addActionListener(this);	
+		bTasty.addActionListener(this);	
+		bFoodNet.addActionListener(this);
 		
 		try {
 			IMGBackground = ImageIO.read(new File("cooking background.jpeg"));
@@ -134,8 +135,9 @@ public class project6 implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		
 		String TastyUrl = "";
+		String FoodNetUrl = "";
 		
-		if(e.getSource() == bDish) {
+		if(e.getSource() == bTasty) {
 			String recipe = tx_dishsearch.getText();
 			
 			while(recipe.indexOf(" ")!=-1) {
@@ -150,10 +152,35 @@ public class project6 implements ActionListener {
 			
 			tx_recipe.setText(RecipeWebsites.TastyProcedure(TastyUrl));		
 			
+			tx_duration.setText(RecipeWebsites.TastyTime(TastyUrl));
+			
+			tx_rating.setText(RecipeWebsites.TastyServing(TastyUrl));
+			
+			tx_overview.setText(RecipeWebsites.TastyOverview(TastyUrl));
 			
 		}
 		
-	
+		if(e.getSource() == bFoodNet) {
+			String recipe = tx_dishsearch.getText();
+			
+			while(recipe.indexOf(" ")!=-1) {
+				recipe = recipe.substring(0,recipe.indexOf(" "))+"-"+recipe.substring(recipe.indexOf(" ")+1);
+			}
+			recipe = recipe.toLowerCase();
+			FoodNetUrl = "https://www.foodnetwork.com/recipes/food-network-kitchen/" + recipe;
+			
+			tx_title.setText(RecipeWebsites.NetworkTitle(FoodNetUrl));		
+			
+			tx_ingredients.setText(RecipeWebsites.NetworkIngredients(FoodNetUrl));		
+			
+			tx_recipe.setText(RecipeWebsites.NetworkProcedure(FoodNetUrl));		
+			
+			tx_duration.setText(RecipeWebsites.NetworkTime(FoodNetUrl));
+			
+			tx_rating.setText(RecipeWebsites.NetworkServing(FoodNetUrl));
+			
+			tx_overview.setText(RecipeWebsites.NetworkOverview(FoodNetUrl));
+		}
 	}
 	
 	public static void main(String[] args) {
